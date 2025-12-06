31486549d43a01cf7374cdacc4cfcf7a47b9b847
poxitron <poxitron1@gmail.com>
2025-12-07 00:38:44 +0100

    Update mount_iso.bat
    Removed french comments

--- IsoMounter/mount_iso.bat
+++ IsoMounter/mount_iso.bat
@@ -1,15 +1,15 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Chemin vers IsoMounter.exe // Path to IsoMounter.exe
+:: Path to IsoMounter.exe
 for %%i in ("%cd%\..\..\..\..\plugins\IsoMounter\IsoMounter.exe") do set "IsoMounter_path=%%~fi"
 
-:: Vérifier si le fichier existe / Check if the file exists
+:: Check if the file exists
 if not exist "%IsoMounter_path%" (
     exit /b 1
 )
 
-:: Lancer le montage de l'ISO // Start mounting the ISO
+:: Mount the ISO
 "%IsoMounter_path%" %*
 
 endlocal
